59df5a5c2dcac185e1cbded8fbc9ef7e15084f1d
Erik Stor <erik.stor.franco@gmail.com>
2022-02-11 16:08:13 -0500

    Hapends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as introJs from 'intro.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'last-try';
+export class AppComponent implements OnInit{
+  
+
+  constructor(){}
+
+  ngOnInit(): void {
+    introJs().addHints();
+  }
+
+  startTour() {
+
+    var intro: any = introJs().addHints();
+    
+    intro.setOptions({
+      steps: [
+        {
+          element: '#tool1',
+          intro: 'Aca es el paso 1',
+          position: 'top',
+        },
+        {
+          element: '#tool2',
+          intro: 'Mor aca es el paso 2',
+          position: 'bottom',
+        },
+        {
+          element: '#tool3',
+          intro: 'Mor aca es el paso 3',
+          position: 'left',
+        },
+        {
+          element: '#tool4',
+          intro: 'Mor aca es el paso 4',
+          position: 'right',
+        },
+      ],
+      showBullets: false,
+      showButtons: true,
+      exitOnOverlayClick: true,
+      keyboardNavigation: true,
+    });
+
+    
+    intro.start();
+  }
 }
